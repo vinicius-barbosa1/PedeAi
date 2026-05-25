b5953e5a58157de677c596baececfd4b31f6e682
Vinicius <vinybarbosa256@gmail.com>
2026-05-25 15:45:11 -0300

    Finalização do controller de Pagamento

--- src/main/java/br/com/ajufood/pedeai/config/ModelMapperConfig.java
+++ src/main/java/br/com/ajufood/pedeai/config/ModelMapperConfig.java
@@ -1,6 +1,8 @@
 package br.com.ajufood.pedeai.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +14,9 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
-
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        // Define a estratégia para Strict, o que evita a sobrescrita acidental de IDs.
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
 }
